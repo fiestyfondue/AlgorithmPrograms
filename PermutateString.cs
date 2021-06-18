50495e887023e93e907aa8da96bbf671b50e2679
Harsh <harsh.21483@gmail.com>
2021-06-18 10:45:19 -0700

    [Harsh]Add:- New class PermutateString is added

--- PermutateString.cs
+++ PermutateString.cs
@@ -6,6 +6,52 @@ namespace Algorithm
 {
     class PermutateString
     {
+        public static void PermuteRecursive(string inputWord, string ans)
+        {
+            if (inputWord.Length == 0)
+            {
+                Console.WriteLine(ans + " ");
+                return;
+            }
+            for (int i = 0; i < inputWord.Length; i++)
+            {
+                char ch = inputWord[i];
+                string leftSubStr = inputWord.Substring(0, i);
+                string rightSubStr = inputWord.Substring(i + 1);
+                string rest = leftSubStr + rightSubStr;
+                PermuteRecursive(rest, ans + ch);
+            }
+        }
         
+        /// <param name="inputword">input sting</param>
+        /// <param name="l">left index</param>
+        /// <param name="r">right index</param>
+        public static void PermuteIterative(string inputword, int l, int r)   // Permutation of string using iterative method 
+        {
+            if (l == r)
+                Console.WriteLine(inputword);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    inputword = Swap(inputword, l, i);
+                    PermuteIterative(inputword, l + 1, r);
+                    inputword = Swap(inputword, l, i);
+                }
+            }
+        }
+      
+        
+        private static string Swap(string inputword, int l, int i)    // swapping method  string
+        {
+            char temp;
+            char[] charArray = inputword.ToCharArray();
+            temp = charArray[l];
+            charArray[l] = charArray[i];
+            charArray[i] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,54 +6,13 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 1, 5, 0, 3, 36, 9 };
-            
-            Array.Sort(numbers);
-            var index = BinarySearch(numbers, 3);
-            
-            Console.WriteLine(index);
-            Console.ReadKey();
+            Console.WriteLine("Algorithm problems");
+            PermutateString permute = new PermutateString();
+            string input = "abcd";
+            PermutateString.PermuteRecursive(input, "");
+            Console.WriteLine("-----------------------------");
+            PermutateString.PermuteIterative(input, 0, input.Length - 1);
+
         }
-            
-
-        private static int LinearSearch(int[] array, int item)  //array and item_we_want_from_array stored
-        {
-
-            for (int i=0; i< array.Length; i++)
-            {
-
-                 if(array[i] == item)
-                 {
-                    return i;
-                 }
-            }
-             return -1;
-        }
-
-        private static int BinarySearch(int[] array, int item )
-        {
-            int left = 0;
-            int right = array.Length - 1;
-
-            while(left<=right)
-            {
-                var middle = (left + right) / 2;  //Slicing the array in half
-
-                if (array[middle] == item)
-                    return middle;
-                    
-
-                if (item < array[middle])
-                {
-                    right = middle - 1;
-
-                }
-                else
-                    left = middle + 1;
-            }
-
-            return -1;
-
-        }   
     }
 }
